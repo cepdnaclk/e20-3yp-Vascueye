30fe202907ff7ae8f71638bb24f3bfe869e05260
tharushika0418 <e20300@eng.pdn.ac.lk>
2025-05-22 21:33:38 +0530

    add testing to backend

--- code/web-frontend/jest.setup.js
+++ code/web-frontend/jest.setup.js
@@ -1,6 +1,10 @@
-// jest.setup.js
 import '@testing-library/jest-dom';
 import fetchMock from 'jest-fetch-mock';
 
 fetchMock.enableMocks();
 global.fetch = fetchMock;
+
+// ✅ Add this polyfill for Node environments
+const { TextEncoder, TextDecoder } = require('util');
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
