e003dc01ec212b99be07578b6ddc4ddc1bb3411b
tgcjananga <150841617+tgcjananga@users.noreply.github.com>
2025-07-16 00:53:06 +0530

    succefully doctor id and pushtoken recieved to the backend

--- code/mobile-app/Backend/controller/pushTokenController.js
+++ code/mobile-app/Backend/controller/pushTokenController.js
@@ -1,14 +1,17 @@
 const savedTokens = new Set();
 
 exports.savePushToken = (req, res) => {
-  const { token } = req.body;
-  if (!token) {
-    return res.status(400).json({ message: "Push token is required" });
+  const { token,doctorId } = req.body;
+  if (!token || !doctorId) {
+    return res.status(400).json({ 
+      message: "Both token and doctorId are required" 
+    });
   }
 
   if (!savedTokens.has(token)) {
     savedTokens.add(token);
     console.log("✅ Push token saved:", token);
+    console.log("✅ Doctor ID recieved:", doctorId);
   }
 
   res.status(200).json({ message: "Push token saved" });
